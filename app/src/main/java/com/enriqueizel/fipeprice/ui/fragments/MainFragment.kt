0e8c991bf0cdd0138a9c69f43b571306b7614054
EnriqueIzel2 <eleaoizel@gmail.com>
2023-05-26 22:11:54 -0400

    feat: add binding

--- app/src/main/java/com/enriqueizel/fipeprice/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/enriqueizel/fipeprice/ui/fragments/MainFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.enriqueizel.fipeprice.R
+import com.enriqueizel.fipeprice.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+  private var _binding: FragmentMainBinding? = null
+  private val binding get() = _binding!!
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_main, container, false)
+    _binding = FragmentMainBinding.inflate(inflater, container, false)
+    val view = binding.root
+
+    return view
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    _binding = null
   }
 }
